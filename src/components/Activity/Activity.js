0db7b5dc5708bf7709aa016beacd8e4eb9a23519
JubayerDev <ajashes858@gmail.com>
2022-11-17 03:14:27 +0600

    activity created name and pic

--- src/components/Activity/Activity.js
+++ src/components/Activity/Activity.js
@@ -2,8 +2,14 @@ import React from 'react';
 
 const Activity = () => {
     return (
-        <div className='sticky top-0'>
-            <h1>Hello</h1>
+        <div className='sticky top-0 pt-10 pl-6'>
+            <div className='flex items-center space-x-3'>
+                <img className='w-12 rounded-full' src="https://placeimg.com/80/80/people" alt='' />
+                <span>
+                    <h5 className='text-lg font-semibold'>Jubayer Hossain</h5>
+                    <small className='text-gray-600 text-sm'>Sylhet, Bangladesh</small>
+                </span>
+            </div>
         </div>
     );
 };
--- src/components/WorkOut/WorkOut.js
+++ src/components/WorkOut/WorkOut.js
@@ -3,13 +3,14 @@ import React from 'react';
 const WorkOut = ({ workout }) => {
     const { id, name, img, desc, time, age } = workout;
     return (
-        <div className='h-80 w-60 p-3 rounded-md bg-white'>
+        <div className='h-96 w-60 p-3 rounded-md bg-white relative'>
             <img className='rounded-md' src={img} alt="workout" />
             <div>
                 <h5 className='text-lg font-semibold text-gray-900 py-3'>{name}</h5>
                 <p className='text-gray-500 font-normal mb-3'>{desc}</p>
                 <p className='text-slate-800 font-medium mb-2'>Age Required: {age}</p>
-                <p className='text-slate-800 font-medium'>Time Required: { time}s</p>
+                <p className='text-slate-800 font-medium'>Time Required: {time}s</p>
+                <button className='bg-[#0d1b2a] py-2 w-[calc(100%-10%)] text-[#faf8f8] font-medium rounded-md absolute bottom-4 left-3'>Add to list</button>
             </div>
         </div>
     );
